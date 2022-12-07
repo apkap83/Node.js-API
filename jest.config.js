eaa4b3e4eb50655936ba24e11de52a648c1a2b23
apkap83 <33512036+apkap83@users.noreply.github.com>
2022-12-07 14:53:07 +0200

    OK

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,6 @@
-const aliases = require('module-alias-jest/register');
-
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
+    testMatch: ['testssd/*test.(js|ts|tsx)'],
     preset: 'ts-jest',
-    transform: {
-        '^.+\\.(ts|tsx)?$': 'ts-jest',
-        '^.+\\.(js|jsx)$': 'babel-jest',
-    },
-    moduleNameMapper: aliases.jest,
+    testEnvironment: 'node',
 };
